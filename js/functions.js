5753c957f93dc37e7ac8c342eaef07f040f32623
cking0987 <cking0987@gmail.com>
2017-08-02 23:42:41 -0500

    added dropdown functionality and 3 helpers to volume input

--- js/functions.js
+++ js/functions.js
@@ -5,6 +5,9 @@ var dankFactorButton = document.getElementById("dankFactorButton");
 var dankFactorOutput = document.getElementById("dankFactorOutput");
 var dankFactorBar = document.getElementById("dankFactorBar");
 
+
+//
+/// Calculate DF
 dankFactorButton.onclick = function() {
   var dankFactorExtended = parseFloat(inputPrice.value) / parseFloat(inputVolume.value) / parseFloat(inputProof.value) * 634.51776 - 3;
   dankFactorOutput.innerHTML = dankFactorExtended.toFixed(2);
@@ -18,6 +21,9 @@ dankFactorButton.onclick = function() {
     dankFactorBar.className = 'progress-bar progress-bar-striped progress-bar-danger';
   }
 };
+
+//
+/// Reset Form
 document.getElementById("resetAll").onclick = function() {
   document.getElementById("dankFactorOutputContainer").style.display = "none";
   dankFactorBar.className = 'progress-bar progress-bar-striped';
@@ -25,3 +31,21 @@ document.getElementById("resetAll").onclick = function() {
   inputProof.value = 'Percent Alcohol';
   inputPrice.value = 'Price';
 };
+
+//
+/// Dropdown functionality
+$('#dropdown-trigger-volume').click(function(){
+    $('#dropdown-content-volume').slideToggle(400);
+});
+
+//
+/// Volume helpers
+$('#volume-helper-wineBottle').click(function(){
+    inputVolume.value = '25';
+});
+$('#volume-helper-beerBottle').click(function(){
+    inputVolume.value = '12';
+});
+$('#volume-helper-12Pack').click(function(){
+    inputVolume.value = '144';
+});
