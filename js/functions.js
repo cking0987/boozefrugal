62a62cc90f7964bac3f0e4c1f6a2ed3cf9975b62
cking0987 <cking0987@gmail.com>
2017-08-12 20:52:20 -0500

    completely redid BF

--- js/functions.js
+++ js/functions.js
@@ -1,51 +1,91 @@
 var inputVolume = document.getElementById("inputVolume");
-var inputProof = document.getElementById("inputProof");
+var inputProof = document.getElementById("inputABV");
 var inputPrice = document.getElementById("inputPrice");
-var dankFactorButton = document.getElementById("dankFactorButton");
-var dankFactorOutput = document.getElementById("dankFactorOutput");
-var dankFactorBar = document.getElementById("dankFactorBar");
-
 
 //
-/// Calculate DF
-dankFactorButton.onclick = function() {
-  var dankFactorExtended = parseFloat(inputPrice.value) / parseFloat(inputVolume.value) / parseFloat(inputProof.value) * 634.51776 - 3;
-  dankFactorOutput.innerHTML = dankFactorExtended.toFixed(2);
-  document.getElementById("dankFactorOutputContainer").style.display = "block";
-  dankFactorBar.style.width = dankFactorExtended.toFixed(2) + "%";
-  if (dankFactorExtended <= 33) {
-    dankFactorBar.className = 'progress-bar progress-bar-striped progress-bar-success';
-    } else if (dankFactorExtended <= 66){
-    dankFactorBar.className = 'progress-bar progress-bar-striped progress-bar-warning';
-    } else {
-    dankFactorBar.className = 'progress-bar progress-bar-striped progress-bar-danger';
-  }
-};
-
-//
-/// Reset Form
-document.getElementById("resetAll").onclick = function() {
-  document.getElementById("dankFactorOutputContainer").style.display = "none";
-  dankFactorBar.className = 'progress-bar progress-bar-striped';
-  inputVolume.value = 'Volume';
-  inputProof.value = 'Percent Alcohol';
-  inputPrice.value = 'Price';
-};
-
-//
-/// Dropdown functionality
+/// Dropdown functionality- volume
 $('#dropdown-trigger-volume').click(function(){
     $('#dropdown-content-volume').slideToggle(400);
+    $(this).find('img').toggle();
+});
+/// Volume helper options
+$('.input-option-vol').click(function(){
+  var volHelper = $(this).data('ref');
+  inputVolume.value = volHelper;
+  ///toggle active image
+    if ($(this).hasClass('inactive')) {
+      $('#dropdown-content-volume .active').not(this).hide();
+      $('#dropdown-content-volume .inactive').not(this).show();
+      $(this).hide();
+      $(this).parent().find('.active').css("display", "inline");
+    };
 });
 
 //
-/// Volume helpers
-$('#volume-helper-wineBottle').click(function(){
-    inputVolume.value = '25';
+/// Dropdown functionality- abv
+$('#dropdown-trigger-abv').click(function(){
+    $('#dropdown-content-abv').slideToggle(400);
+    $(this).find('img').toggle();
 });
-$('#volume-helper-beerBottle').click(function(){
-    inputVolume.value = '12';
+/// ABV helper options
+$('.input-option-abv').click(function(){
+  var abvHelper = $(this).data('ref');
+  inputABV.value = abvHelper;
+  ///toggle active image
+  if ($(this).hasClass('inactive')) {
+    $('#dropdown-content-abv .active').not(this).hide();
+    $('#dropdown-content-abv .inactive').not(this).show();
+    $(this).hide();
+    $(this).parent().find('.active').css("display", "inline");
+  };
 });
-$('#volume-helper-12Pack').click(function(){
-    inputVolume.value = '144';
+
+//
+/// Calculate button
+$('#dropdown-trigger-calc1').click(function(){
+    if ( (/^\d+$/.test($('#inputVolume').val())) && (/^\d+$/.test($('#inputABV').val())) && (/^\d+$/.test($('#inputPrice').val())) ) {
+      //clear errors
+      $('#errorMessage').html('');
+      //dropdown
+
+      if( !$('#dropdown-content-calc1').is(':visible') ) {
+        $('#dropdown-content-calc1').slideToggle(400);
+      };
+      //show reset button
+        $('#bfFormResetAll').show();
+      //calculate ppb
+      var ppbCurrent = ( parseFloat(inputPrice.value) / parseFloat(inputVolume.value) / parseFloat(inputProof.value) * 100 ).toFixed(2);
+      console.log('just calculated ppb:' + ppbCurrent);
+      //show ppb in case it was hidden by an error
+      $('.ppbOutput').show();
+      $('#ppbValue').html(ppbCurrent);
+    } else {
+      //hide ppb
+      $('.ppbOutput').hide();
+      //dropdown
+      if( !$('#dropdown-content-calc1').is(':visible') ) {
+        $('#dropdown-content-calc1').slideToggle(400);
+      };
+      //set error message
+      $('#errorMessage').html('Hmm.... Something ain\'t quite right upstairs. Make sure each field has a valid number.');
+      console.log('form validation failed');
+    };
+});
+
+$('#bfFormResetAll').click(function(){
+  inputVolume.value = '';
+  inputABV.value = '';
+  inputPrice.value = '';
+  $('#ppbValue').html('');
+  $('#errorMessage').html('');
+  if( $('#dropdown-content-calc1').is(':visible') ) {
+    $('#dropdown-content-calc1').slideToggle(400);
+  };
+  console.log('form has been reset');
+});
+
+//
+/// Dropdown functionality- calc2
+$('#dropdown-trigger-calc2').click(function(){
+    $('#dropdown-content-calc2').slideToggle(400);
 });
